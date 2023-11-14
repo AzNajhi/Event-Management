633c9b95737ac4b1dd8fd9b5f2e897d3560961b1
aznajhi <aznajhi97games@gmail.com>
2023-11-14 14:59:17 +0800

    Update main file

--- Event Management System/main.py
+++ Event Management System/main.py
@@ -2,12 +2,9 @@ from tkinter import *
 import options
 
 if __name__ == "__main__":
-    try:
-        root = Tk()
-        root.title("Mezza9 Solutions")
-        root.iconbitmap(r"Icon\Mezza9-Icon.ico")
-        options.options(root)
-        root.mainloop()
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        input("Press Enter to exit...")
+    root = Tk()
+    root.title("Mezza9 Solutions")
+    root.iconbitmap(r"Icon\Mezza9-Icon.ico")
+    options.options(root)
+    root.mainloop()
+
